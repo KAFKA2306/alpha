607704c8cf6d6b9fb8d3f19de1a1a4b1befafd16
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-09-12 10:51:28 +0900

    test: enforce executable default block registry

--- tests/test_registry_smoke.py
+++ tests/test_registry_smoke.py
@@ -6,13 +6,31 @@ from models.domain_blocks import get_domain_block_registry
 
 
 class RegistrySmokeTest(unittest.TestCase):
-    def test_default_registry_executes_layer_norm(self):
-        registry = get_domain_block_registry()
-        self.assertEqual(len(registry.get_all_blocks()), 15)
+    canonical_shape = (2, 16, 8)
 
-        module = registry.get_block("layer_norm").create_module((2, 4, 3))
-        output = module(torch.randn(2, 4, 3))
-        self.assertEqual(tuple(output.shape), (2, 4, 3))
+    def test_default_registry_executes_every_registered_block(self):
+        registry = get_domain_block_registry()
+        blocks = registry.get_all_blocks()
+        self.assertEqual(len(blocks), 15)
+
+        for block in blocks:
+            with self.subTest(block=block.name):
+                self.assertTrue(block.validate_input_shape(self.canonical_shape))
+                module = block.create_module(self.canonical_shape)
+                module.eval()
+                output = module(torch.randn(*self.canonical_shape))
+                expected_shape = tuple(block.get_output_shape(self.canonical_shape))
+                self.assertEqual(tuple(output.shape), expected_shape)
+
+    def test_time_mixing_conv_preserves_channel_last_shape(self):
+        registry = get_domain_block_registry()
+        block = registry.get_block("time_mixing")
+        module = block.create_module(self.canonical_shape, mixing_type="conv")
+        output = module(torch.randn(*self.canonical_shape))
+        self.assertEqual(
+            tuple(output.shape),
+            tuple(block.get_output_shape(self.canonical_shape, mixing_type="conv")),
+        )
 
 
 if __name__ == "__main__":
